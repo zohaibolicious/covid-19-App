c6a8998c2d596756807f704756cc2761d8ed3467
zohaibolicious <zabimalik098@gmail.com>
2020-04-19 00:24:00 +0500

    Updates

---
+++ precache-manifest.abb92c8efe6aa94e3eb551034b5b1951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74b03d19ea8b1d7c5ca1ec20a32a21e",
+    "url": "/zohaibolicious.github.io/covid-19-App/index.html"
+  },
+  {
+    "revision": "131b6440a1f6d5862e1d",
+    "url": "/zohaibolicious.github.io/covid-19-App/static/css/main.9ef2680b.chunk.css"
+  },
+  {
+    "revision": "4952916557a1551b5d85",
+    "url": "/zohaibolicious.github.io/covid-19-App/static/js/2.e9c42838.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/zohaibolicious.github.io/covid-19-App/static/js/2.e9c42838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131b6440a1f6d5862e1d",
+    "url": "/zohaibolicious.github.io/covid-19-App/static/js/main.18f05dbf.chunk.js"
+  },
+  {
+    "revision": "4c70054d8d237c450000",
+    "url": "/zohaibolicious.github.io/covid-19-App/static/js/runtime-main.3e216804.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/zohaibolicious.github.io/covid-19-App/static/media/image.d7265326.png"
+  }
+]);
